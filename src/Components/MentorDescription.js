91ed529778cffe961e1f4b0ec1be6b71bfd802f7
Silavsale <silavsale@gmail.com>
2024-04-07 12:18:51 -0400

    updated translation

--- src/Components/MentorDescription.js
+++ src/Components/MentorDescription.js
@@ -1,43 +1,34 @@
 import React from 'react';
 import { Box, Heading, Text, List, ListItem, ListIcon } from '@chakra-ui/react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
+import { useTranslation } from 'react-i18next';
 
 function MentorDescriptionShort() {
+  const { t } = useTranslation();
+
   return (
     <Box maxW="xl" p={5} borderWidth="1px" borderRadius="lg" boxShadow="lg">
-      <Heading mb={4}>Why Enroll in My Programming Classes?</Heading>
-      <Text mb={4}>
-        Elevate your tech career with a mentor who has extensive full-stack
-        development experience across startups and big enterprises. My classes
-        go beyond coding to offer a holistic view of the tech industry, tailored
-        to make you job-ready.
-      </Text>
+      <Heading mb={4}>{t('whyEnrollTitle')}</Heading>
+      <Text mb={4}>{t('enrollDescription')}</Text>
       <List spacing={3} mb={4}>
         <ListItem>
           <ListIcon as={CheckCircleIcon} color="green.500" />
-          Real-World Skills: Master web development from code to deployment, as
-          it’s done in top companies.
+          {t('realWorldSkills')}
         </ListItem>
         <ListItem>
           <ListIcon as={CheckCircleIcon} color="green.500" />
-          Professional Growth: Learn Agile, sprint planning, and task management
-          to excel in tech roles.
+          {t('professionalGrowth')}
         </ListItem>
         <ListItem>
           <ListIcon as={CheckCircleIcon} color="green.500" />
-          Career-Focused Curriculum: Gain the skills that employers demand in
-          today’s tech landscape.
+          {t('careerFocusedCurriculum')}
         </ListItem>
         <ListItem>
           <ListIcon as={CheckCircleIcon} color="green.500" />
-          Personalized Guidance: Benefit from my experience to navigate both
-          technical and career challenges effectively.
+          {t('personalizedGuidance')}
         </ListItem>
       </List>
-      <Text>
-        Transform your passion into a profession. Join my classes and chart a
-        path to success in the tech industry.
-      </Text>
+      <Text>{t('transformYourPassion')}</Text>
     </Box>
   );
 }
